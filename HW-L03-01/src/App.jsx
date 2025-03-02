57893804862054da73a1f56efa448297d1597852
ramtinbs <ramtinbashokat@gmail.com>
2025-03-03 02:23:00 +0330

    input value with btn done!

--- HW-L03-01/src/App.jsx
+++ HW-L03-01/src/App.jsx
@@ -17,13 +17,33 @@ function App() {
       description: "Noise-canceling headphones",
     },
   ];
+  const handleClick = () => {
+    const inputValue = document.getElementById("inputField").value;
+    console.log("input value", inputValue);
+  };
   return (
-    <div className="min-h-screen flex flex-col item-center justify-center bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">
-        product List
-      </h1>
-      <ProductList products={products} />
-    </div>
+    <>
+      <div className="min-h-screen flex flex-col item-center justify-center bg-gray-100 p-4 ">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center ">
+          product List
+        </h1>
+        <ProductList products={products} />
+        <div className="p-6 flex flex-col items-center justify-center space-y-4">
+          <input
+            className="border p-2 rounded-md w-64 bg-white border-2 border-sky-500"
+            type="text"
+            id="inputField"
+            placeholder="enter a value"
+          />
+          <button
+            className="bg-blue-500 text-white px-4 rounded-md hover:bg-blue-600 p-[10px] cursor-pointer"
+            onClick={handleClick}
+          >
+            show in console
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- HW-L03-01/src/components/ProductList.jsx
+++ HW-L03-01/src/components/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 const ProductList = ({ products }) => {
   return (
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center border-b-2 p-3">
       {products.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
