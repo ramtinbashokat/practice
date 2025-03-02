cc41d2afa7b5e4757cd1c051e51eb34f25b0b285
ramtinbs <ramtinbashokat@gmail.com>
2025-03-03 02:00:00 +0330

    product List completed with styled

--- HW-L03-01/src/App.jsx
+++ HW-L03-01/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const products = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 999,
+      description: "High-performance laptop",
+    },
+    { id: 2, name: "Phone", price: 699, description: "Latest smartphone" },
+    {
+      id: 3,
+      name: "Headphones",
+      price: 199,
+      description: "Noise-canceling headphones",
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex flex-col item-center justify-center bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4 text-center">
+        product List
+      </h1>
+      <ProductList products={products} />
+    </div>
+  );
 }
 
-export default App
+export default App;
